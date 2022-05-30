4eb1201f7c83a6b25076e729ada2b5f7e1bcecc2
Komal kumar <kk2082187@gmail.com>
2022-05-30 15:18:10 +0530

    first commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,53 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+          <ul className="fList">
+           <li className="fListsItem">Countries</li>
+           <li className="fListsItem">Regions</li>
+           <li className="fListsItem">Cities</li>
+           <li className="fListsItem">Districts</li>
+           <li className="fListsItem">Airports</li>
+           <li className="fListsItem">hotels</li>
+          </ul>
+          <ul className="fList">
+           <li className="fListsItem">Countries</li>
+           <li className="fListsItem">Regions</li>
+           <li className="fListsItem">Cities</li>
+           <li className="fListsItem">Districts</li>
+           <li className="fListsItem">Airports</li>
+           <li className="fListsItem">hotels</li>
+          </ul>
+          <ul className="fList">
+           <li className="fListsItem">Countries</li>
+           <li className="fListsItem">Regions</li>
+           <li className="fListsItem">Cities</li>
+           <li className="fListsItem">Districts</li>
+           <li className="fListsItem">Airports</li>
+           <li className="fListsItem">hotels</li>
+          </ul>
+          <ul className="fList">
+           <li className="fListsItem">Countries</li>
+           <li className="fListsItem">Regions</li>
+           <li className="fListsItem">Cities</li>
+           <li className="fListsItem">Districts</li>
+           <li className="fListsItem">Airports</li>
+           <li className="fListsItem">hotels</li>
+          </ul>
+          <ul className="fList">
+           <li className="fListsItem">Countries</li>
+           <li className="fListsItem">Regions</li>
+           <li className="fListsItem">Cities</li>
+           <li className="fListsItem">Districts</li>
+           <li className="fListsItem">Airports</li>
+           <li className="fListsItem">hotels</li>
+          </ul>
+      </div>
+      <div className="fText">Copyright @ 2022 Booking.</div>
+    </div>
+  )
+}
+
+export default Footer
